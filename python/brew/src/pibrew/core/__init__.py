ad7600a3632575c9300e61404247705acc4fe600
Bjorn Andersson <bjorn.andersson@gpsgate.com>
2025-05-31 00:24:18 +0200

    Removed failed test

--- python/brew/src/pibrew/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core brewing control logic."""
-
-from .brew_controller import BrewController
-from .pid_controller import PIDController
-
-__all__ = ["BrewController", "PIDController"]
--- python/brew/src/pibrew/web/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Web interface for PiBrew."""
-
-from .server import create_app, run_server
-
-__all__ = ["create_app", "run_server"]
